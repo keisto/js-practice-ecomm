2e47485c4761219abddd4d589b36308088180e5b
Tony Keiser <keisertony@gmail.com>
2023-01-26 16:38:23 -0800

    UsersRepositories

---
+++ repositories/users.js
@@ -0,0 +1,83 @@
+const fs = require('fs')
+const crypto = require('crypto')
+
+class UsersRepository {
+  constructor(filename) {
+    if (!filename) {
+      throw new Error('Creating a repository requires a filename')
+    }
+
+    this.filename = filename
+    try {
+      fs.accessSync(this.filename)
+    } catch (err) {
+      fs.writeFileSync(this.filename, '[]')
+    }
+  }
+
+  async getAll() {
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, {
+        encoding: 'utf-8',
+      })
+    )
+  }
+
+  async create(attrs) {
+    const records = await this.getAll()
+    records.push({ id: this.randomId(), ...attrs })
+
+    await this.writeAll(records)
+  }
+
+  async getOne(id) {
+    const records = await this.getAll()
+    return records.find((record) => record.id === id) || null
+  }
+
+  async delete(id) {
+    const records = await this.getAll()
+    await this.writeAll(records.filter((record) => record.id !== id))
+  }
+
+  async update(id, attrs) {
+    const records = await this.getAll()
+    const record = records.find((record) => record.id === id)
+
+    if (!record) {
+      throw new Error(`Record with ID: ${id} not found.`)
+    }
+
+    Object.assign(record, attrs)
+    await this.writeAll(records)
+  }
+
+  async getOneBy(filters) {
+    const records = await this.getAll()
+
+    for (const record of records) {
+      let found = true
+      for (let key in filters) {
+        if (record[key] !== filters[key]) {
+          found = false
+        }
+      }
+
+      if (found) {
+        return record
+      }
+    }
+
+    return null
+  }
+
+  async writeAll(records) {
+    await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2))
+  }
+
+  randomId() {
+    return crypto.randomBytes(4).toString('hex')
+  }
+}
+
+module.export = new UsersRepository('users.json')
