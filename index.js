489f7f16731fa1d8ff1dd49d2ea500542662ca8c
Tony Keiser <keisertony@gmail.com>
2023-01-27 16:42:25 -0800

    Authentication using cookies

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const userRepo = require('./repositories/users')
+const cookieSession = require('cookie-session')
+const usersRepo = require('./repositories/users')
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(
+  cookieSession({
+    keys: ['571744f3-f3de-4c18-8174-ad4dbcbd265a-tk-oss'],
+  })
+)
 
-app.get('/', (req, res) => {
+app.get('/signup', (req, res) => {
   res.send(`
     <div>
+       ${req.session.userId}
       <form method="post">
         <input type="text" placeholder="Email" name="email"/>
         <input type="password" placeholder="Password" name="password">
@@ -18,9 +25,9 @@ app.get('/', (req, res) => {
   `)
 })
 
-app.post('/', async (req, res) => {
+app.post('/signup', async (req, res) => {
   const { email, password, passwordConfirmation } = req.body
-  const existingUser = await userRepo.getOneBy({ email })
+  const existingUser = await usersRepo.getOneBy({ email })
 
   if (existingUser) {
     return res.send('Email in use.')
@@ -30,7 +37,50 @@ app.post('/', async (req, res) => {
     return res.send('Passwords do not match.')
   }
 
+  // Create user
+  const { id } = await usersRepo.create({ email, password })
+
+  // Store id of user in a cookie
+  req.session.userId = id
+
   res.send('Account created!')
+})
+
+app.get('/signout', (req, res) => {
+  req.session = null
+  res.send('You are logged out')
+})
+
+app.get('/signin', (req, res) => {
+  res.send(`
+    <div>
+      <form method="post">
+        <input type="text" placeholder="Email" name="email"/>
+        <input type="password" placeholder="Password" name="password">
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
+  `)
+})
+
+app.post('/signin', async (req, res) => {
+  const { email, password } = req.body
+  const user = await usersRepo.getOneBy({ email })
+
+  if (!user) {
+    return res.send(`User not found with this email: ${email}`)
+  }
+
+  const validPassword = await usersRepo.comparePasswords(
+    user.password,
+    password
+  )
+  if (!validPassword) {
+    return res.send('Invalid password.')
+  }
+
+  req.session.userId = user.id
+  res.send('You are signed in!')
 })
 
 app.listen(3000, () => {
--- repositories/users.js
+++ repositories/users.js
@@ -1,5 +1,8 @@
 const fs = require('fs')
 const crypto = require('crypto')
+const util = require('util')
+
+const scrypt = util.promisify(crypto.scrypt)
 
 class UsersRepository {
   constructor(filename) {
@@ -24,10 +27,19 @@ class UsersRepository {
   }
 
   async create(attrs) {
+    attrs.id = this.randomId()
+
+    const salt = crypto.randomBytes(8).toString('hex')
+    const buf = await scrypt(attrs.password, salt, 64)
+    const hashed = `${buf.toString('hex')}.${salt}`
+
     const records = await this.getAll()
-    records.push({ id: this.randomId(), ...attrs })
+    const record = { ...attrs, password: hashed }
+    records.push(record)
 
     await this.writeAll(records)
+
+    return record
   }
 
   async getOne(id) {
@@ -69,6 +81,14 @@ class UsersRepository {
     }
 
     return null
+  }
+
+  async comparePasswords(savedHashedPassword, suppliedPassword) {
+    const [hashed, salt] = savedHashedPassword.split('.')
+    const buf = await scrypt(suppliedPassword, salt, 64)
+    const hashedSupplied = buf.toString('hex')
+
+    return hashed === hashedSupplied
   }
 
   async writeAll(records) {
