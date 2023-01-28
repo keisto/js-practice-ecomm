bfbec6590750db9359e22efcafc5bd176aa92959
Tony Keiser <keisertony@gmail.com>
2023-01-27 18:41:01 -0800

    Added styling with Bulma

--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -4,18 +4,35 @@ const { getError } = require('../../../views/helpers')
 module.exports = ({ req, errors }) => {
   return layout({
     content: `
-    <div>
-       ${req.session.userId}
-      <form method="post">
-        <input type="text" placeholder="Email" name="email"/>
-        ${getError(errors, 'email')}
-        <input type="password" placeholder="Password" name="password">
-        ${getError(errors, 'password')}
-        <input type="password" placeholder="Password Confirmation" name="passwordConfirmation">
-        ${getError(errors, 'passwordConfirmation')}
-        <button type="submit">Sign Up</button>
-      </form>
-    </div>
-  `,
+      <div class="container">
+        <div class="columns is-centered">
+          <div class="column is-one-quarter">
+            <form method="POST">
+              <h1 class="title">Sign Up</h1>
+              <div class="field">
+                <label class="label">Email</label>
+                <input required class="input" placeholder="Email" name="email" />
+                <p class="help is-danger">${getError(errors, 'email')}</p>
+              </div>
+              <div class="field">
+                <label class="label">Password</label>
+                <input required class="input" placeholder="Password" name="password" type="password" />
+                <p class="help is-danger">${getError(errors, 'password')}</p>
+              </div>
+              <div class="field">
+                <label class="label">Password Confirmation</label>
+                <input required class="input" placeholder="Password Confirmation" name="passwordConfirmation" type="password" />
+                <p class="help is-danger">${getError(
+                  errors,
+                  'passwordConfirmation'
+                )}</p>
+              </div>
+              <button class="button is-primary">Submit</button>
+            </form>
+            <a href="/signin">Have an account? Sign In</a>
+          </div>
+        </div>
+      </div>
+    `,
   })
 }
--- views/admin/layout.js
+++ views/admin/layout.js
@@ -7,9 +7,33 @@ module.exports = ({ content }) => {
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Ecomm</title>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" rel="stylesheet">
+        <link href="/css/main.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css"></link>
       </head>
       <body>
-        ${content}
+        <body class="admin">
+        <header>
+          <nav class="navbar navbar-bottom">
+            <div class="container navbar-container">
+              <div>
+                <a href="/admin/products">
+                  <h3 class="title">Admin Panel</h3>
+                </a>
+              </div>
+              <div class="navbar-item">
+                <div class="navbar-buttons">
+                  <div class="navbar-item">
+                    <a href="/admin/products"><i class="fa fa-star"></i> Products</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </header>
+        <div class="container">
+          ${content}
+        </div>
       </body>
     </html>
   `
