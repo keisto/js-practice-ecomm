5ad370cb4b6b415639a62dfd875c4a145507b21a
Tony Keiser <keisertony@gmail.com>
2023-01-28 15:23:48 -0800

    Added products delete

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -73,4 +73,10 @@ router.post(
   }
 )
 
+router.post('/admin/products/:id/delete', requireAuth, async (req, res) => {
+  await productsRepo.delete(req.params.id)
+
+  res.redirect('/admin/products')
+})
+
 module.exports = router
